fe5952795f751652fe92c8d97def9f26e6c950d2
Omar-Kamall <ok9192750000000@gmail.com>
2025-04-23 00:23:00 +0200

    Frist Commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,19 +9,19 @@ const Contact = lazy (() => import ('../Components/Contact'));
 const Home = () => {
     return (
         <div>
-            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
+            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center z-100'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
                 <Header className="box"/>
             </Suspense>
-            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
+            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center z-100'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
                 <Service className="box"/>
             </Suspense>
-            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
+            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center z-100'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
                 <Skill className="box"/>
             </Suspense>
-            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
+            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center z-100'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
                 <Portfolio className="box"/>
             </Suspense>
-            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
+            <Suspense fallback={<div className='bg-[#0A0F1F] h-screen text-7xl items-center justify-center z-100'><FadeLoader className='text-[#FF6F91]' size={50} /></div>}>
                 <Contact className="box"/>
             </Suspense>
         </div>
