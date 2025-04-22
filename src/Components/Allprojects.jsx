2e3323933ee5772242b381044d2f21f160f048f2
Omar-Kamall <ok9192750000000@gmail.com>
2025-04-23 00:52:29 +0200

    Frist Commit

--- src/Components/Allprojects.jsx
+++ src/Components/Allprojects.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react'
+import { FadeLoader } from 'react-spinners';
 
 const Allprojects = (props) => {
     const [projects, setProjects] = useState([]);
