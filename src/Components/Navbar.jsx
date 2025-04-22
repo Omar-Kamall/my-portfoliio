891d34182c3023f4d011f1238504e8f63a61494e
Omar-Kamall <ok9192750000000@gmail.com>
2025-04-22 23:13:29 +0200

    Frist Commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,24 +8,28 @@ import { logo } from '../../public/assets';
 const Navbar = () => {
     const location = useLocation();
     const [bar , setBar] = useState(false);
+    const [Active , setactive] = useState(false);
     const [active , setActive] = useState(null);
     const links = [
-        {id: 0 , name: "HOME" , to: location.pathname === "/" ? "/#Home" : "/" },
-        {id: 1 , name: "SERVICE" , to: location.pathname === "/" ? "#service" : "/#service"},
-        {id: 2 , name: "SKILLS" , to: location.pathname === "/" ? "#skills" : "/#skills"},
-        {id: 3 , name: "PORTFOLIO" , to: location.pathname === "/" ? "#portfolio" : "/#portfolio"},
-        {id: 4 , name: "CONTACT" , to: location.pathname === "/" ? "#contact" : "/#contact"}
+        {id: 0 , name: "SERVICE" , to: location.pathname === "/" ? "#service" : "/#service"},
+        {id: 1 , name: "SKILLS" , to: location.pathname === "/" ? "#skills" : "/#skills"},
+        {id: 2 , name: "PORTFOLIO" , to: location.pathname === "/" ? "#portfolio" : "/#portfolio"},
+        {id: 3 , name: "CONTACT" , to: location.pathname === "/" ? "#contact" : "/#contact"}
     ]
     return (
         <nav className='text-white h-10 w-full top-5 z-100 fixed'>
             <div className='container mx-auto px-[5%]'>
                 <div className="flex items-center justify-between bg-[#0A0F1F] border-1 border-gray-600 p-5 rounded-2xl">
-                    <Link className='text-[18px] hover:text-[#4A90E2] transition duration-300' to="/"><div className="flex items-center"><img className='w-15 h-10' src={logo} alt="Image-Error" loading='lazy' />OMAR KAMAL</div></Link>
+                    <Link onClick={() => setactive(true)} className='text-[18px] hover:text-[#4A90E2] transition duration-300' to="/"><div className="flex items-center"><img className='w-15 h-10' src={logo} alt="Image-Error" loading='lazy' />OMAR KAMAL</div></Link>
                     <div className="hidden lg:flex gap-5">
+                        {location.pathname === "/" ?
+                            <HashLink onClick={() => setactive(true)} smooth to="#Home" className={`text-[18px] hover:text-[#4A90E2] ${Active && "text-[#4A90E2]"} transition duration-300`}>HOME</HashLink> : 
+                            <Link onClick={() => setactive(true)} to="/" className={`text-[18px] hover:text-[#4A90E2] ${Active && "text-[#4A90E2]"} transition duration-300`}>HOME</Link>
+                        }
                         {links.map(link => (
-                            <HashLink smooth onClick={() => {setActive(link.id)}} className={`text-[18px] hover:text-[#4A90E2] ${active === link.id && location.pathname === "/" && "text-[#4A90E2]"} transition duration-300`} key={link.id} to={link.to}>{link.name}</HashLink>
+                            <HashLink smooth onClick={() => {setActive(link.id) , setactive(false)}} className={`text-[18px] hover:text-[#4A90E2] ${active === link.id && !Active && (location.pathname !== link.to && location.pathname !== "Home" && location.pathname !== "/projects") && "text-[#4A90E2]"} transition duration-300`} key={link.id} to={link.to}>{link.name}</HashLink>
                         ))}
-                        <Link smooth to="/projects" className={`text-[18px] hover:text-[#4A90E2] ${location.pathname === "/projects" && "text-[#4A90E2]"} transition duration-300`}>PROJECTS</Link>
+                        <Link to="/projects" onClick={() => setactive(false)} className={`text-[18px] hover:text-[#4A90E2] ${location.pathname === "/projects" && "text-[#4A90E2]"} transition duration-300`}>PROJECTS</Link>
                     </div>
                     <div className="lg:hidden">
                         {!bar ? <FaBars onClick={() => setBar(true)} className='hover:text-[#4A90E2] transition duration-300 cursor-pointer' size={25}/> :
@@ -36,10 +40,14 @@ const Navbar = () => {
                 <div className="flex flex-col items-end lg:hidden">
                     {bar &&
                         <div className="flex flex-col gap-2 w-43 mt-2 bg-[#0A0F1F] border-1 border-gray-600 rounded-2xl p-5">
+                            {location.pathname === "/" ?
+                                <HashLink onClick={() => {setBar(false) , setactive(true)}} smooth to="#Home" className={`text-[18px] hover:text-[#4A90E2] ${Active && "text-[#4A90E2]"} transition duration-300`}>HOME</HashLink> :
+                                <Link onClick={() => {setBar(false) , setactive(true)}} to="/" className={`text-[18px] hover:text-[#4A90E2] ${Active && "text-[#4A90E2]"} transition duration-300`}>HOME</Link>
+                            }
                             {links.map(link => (
-                                <HashLink smooth onClick={() => {setActive(link.id) , setBar(false)}} className={`text-[18px] hover:text-[#4A90E2] ${active === link.id && location.pathname === "/" && "text-[#4A90E2]"} transition duration-300`} key={link.id} to={link.to}>{link.name}</HashLink>
+                                <HashLink smooth onClick={() => {setActive(link.id) , setBar(false) , setactive(false)}} className={`text-[18px] hover:text-[#4A90E2] ${active === link.id && location.pathname === "/" && !Active && "text-[#4A90E2]"} transition duration-300`} key={link.id} to={link.to}>{link.name}</HashLink>
                             ))}
-                            <Link smooth to="/projects" className={`text-[18px] hover:text-[#4A90E2] ${location.pathname === "/projects" && "text-[#4A90E2]"} transition duration-300`}>PROJECTS</Link>
+                            <Link smooth onClick={() => {setBar(false) , setactive(false)}} to="/projects" className={`text-[18px] hover:text-[#4A90E2] ${location.pathname === "/projects" && "text-[#4A90E2]"} transition duration-300`}>PROJECTS</Link>
                         </div>
                     }
                 </div>
